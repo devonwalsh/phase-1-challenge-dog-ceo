f6530af6d641458c741b4f4884d3b7db6a4da299
Devon Walsh <devonwalsh@gmail.com>
2021-03-19 22:57:02 -0400

    Finished challenge 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+function getDogs() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        dogPics = json.message;
+        addImages(dogPics);
+    })
+};
+
+function addImages(images) {
+    const dogHouse = document.getElementById("dog-image-container");
+    images.forEach(dog => {
+        const p = document.createElement("p");
+        const img = document.createElement("img");
+        img.src=dog
+        dogHouse.appendChild(img);
+        dogHouse.appendChild(p);
+    })
+}
+
+
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener("DOMContentLoaded", getDogs);
