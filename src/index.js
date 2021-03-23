447f18a72753955ee67c2dcf87af9bbbefa66da3
Devon Walsh <devonwalsh@gmail.com>
2021-03-23 16:16:43 -0400

    Revised code, working on challenge 4

--- src/index.js
+++ src/index.js
@@ -1,55 +1,62 @@
-function getDogs() {
-    fetch("https://dog.ceo/api/breeds/image/random/4")
-    .then(function(response) {
-        return response.json();
+//Event listeners
+document.addEventListener("DOMContentLoaded", () => { 
+    callImageAPI();
+    callBreedAPI();
+    document.getElementById("breed-dropdown").addEventListener("change", (e) => {
+        let dropdownValue = e.target.value;
     })
-    .then(function(json) {
-        let dogPics = json.message;
+})
+
+function callImageAPI() {
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(data => {
+        let dogPics = data.message;
         addImages(dogPics);
     });
 }
 
 function addImages(images) {
-    const dogHouse = document.getElementById("dog-image-container");
+    const dogContainer = document.getElementById("dog-image-container");
     images.forEach(dog => {
         const p = document.createElement("p");
         const img = document.createElement("img");
         img.src=dog;
-        dogHouse.appendChild(img);
-        dogHouse.appendChild(p);
+        dogContainer.appendChild(img);
+        dogContainer.appendChild(p);
     });
 }
 
-function getBreeds() {
+function callBreedAPI() {
     fetch("https://dog.ceo/api/breeds/list/all")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(json) {
-        let breedArray = json.message;
-        addBreeds(breedArray);
+    .then(response => response.json())
+    .then(data => {
+        let breedObject = data.message;
+        addBreedList(breedObject);
     });
 }
 
-function addBreeds(breedJSON) {
-    const breedList = document.getElementById("dog-breeds");
+function addBreedList(breedObject) {
+    const breedContainer = document.getElementById("dog-breeds");
     
-    for (let breed in breedJSON) {
-        let breedText;
+    for (let breed in breedObject) {
+        let breedList;
         const li = document.createElement("li");
-        if (breedJSON[breed].length > 0) {
-            breedText = document.createTextNode(`${breed}: ${(breedJSON[breed]).join(', ')}`)
+        if (breedObject[breed].length > 0) {
+            breedList = document.createTextNode(`${breed}: ${(breedObject[breed]).join(', ')}`)
         }
-        else breedText = document.createTextNode(breed);
-        li.onclick = changeColor;
-        li.appendChild(breedText);
-        breedList.appendChild(li);
+        else breedList = document.createTextNode(breed);
+        li.appendChild(breedList);
+        breedContainer.appendChild(li);
     }
+    addEventListeners();
 }
 
-function changeColor() {
+function changeTextColor() {
     this.style.color = "blue";
 }
 
-document.addEventListener("DOMContentLoaded", getDogs);
-document.addEventListener("DOMContentLoaded", getBreeds);
+function addEventListeners() {
+    const lis = document.querySelectorAll("li")
+    lis.forEach(li => li.addEventListener("click", changeTextColor))
+}
